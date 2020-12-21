26d2f9d97a838f43a7bf909fe5a86516dda8dce2
0404btruong <0404btruong@gmail.com>
2020-12-20 19:04:00 -0800

    Made the website exist lol (Added base route)

--- Main.py
+++ Main.py
@@ -5,10 +5,14 @@ from flask import Flask, render_template, url_for, request, redirect
 #create a Flask instance
 app = Flask(__name__)
 
+@app.route('/base/')
+def base_route():
+    return render_template("base.html", projects=projects.setup())
+
 @app.route('/')
 def home_route():
     return render_template("home.html", projects=projects.setup())
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(port='3000', host='0.0.0.0')
+    app.run(port='5000', host='127.0.0.1')
