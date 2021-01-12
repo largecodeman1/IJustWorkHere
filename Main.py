7d475fb3b7e7f8b640c0083beaa5a8f08a7f7bab
zachajoseph <zachajoseph@gmail.com>
2021-01-12 00:31:06 -0600

    Creating test run

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/base/')
 def base_route():
-    return render_template("index.html", projects=projects.setup())
+    return render_template("about.html", projects=projects.setup())
 
 @app.route('/')
 def home_route():
