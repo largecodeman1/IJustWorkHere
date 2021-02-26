43cade193ce67047f0aefa9caacafc2265dcf1a2
0404btruong <0404btruong@gmail.com>
2021-02-26 11:56:38 -0800

    api update

--- API_test.py
+++ API_test.py
@@ -12,7 +12,7 @@ AccountId = 'psy6'
 #
 # NOTE: NEED TO UPDATE THIS EVERY 7 HOURS - Call Zach or create your own account on developer.riotgames.com
 #
-API_Key = "RGAPI-7e388eac-9a63-4976-b676-0584fd1bcfa1"
+API_Key = "RGAPI-b7639728-d910-46be-a5b7-369de2467267"
 
 # Get Riot test data for mid-term and after put into database
 def APIQuery(AccountId):
